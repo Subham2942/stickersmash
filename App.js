608334cc7217e62d6046edca9be8e65b77203636
Subham2942 <subhamdassneha@gmail.com>
2024-08-12 01:04:16 +0530

    message

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker'
+
+import ImageViewer from "./components/ImageViewer";
+import placeholderImage from './assets/placeholder.png'
+import Button from "./components/Button";
+
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert('You did not select any image.');
+      setSelectedImage(null)
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={placeholderImage} selectedImage={selectedImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Choose a photo"} theme="primary" onPress={pickImageAsync} />
+        <Button label={"Use this photo"}  />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +41,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  imageContainer: {
+    paddingTop: 58,
+    flex: 1
+  },
+  footerContainer:{
+    alignItems: 'center',
+    flex: 1/3
+  }
 });
